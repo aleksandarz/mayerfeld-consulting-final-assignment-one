45bd83621bb31f8fbe2dea6bbc036b154bb5021e
zuletaxi <julka.zet97@gmail.com>
2026-04-22 22:54:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const displayElement = document.getElementById('display');
+const numberButtons = document.querySelectorAll('.number-btn');
+const decimalButton = document.getElementById('btn-decimal');
+const clearButton = document.getElementById('btn-clear');
+const backspaceButton = document.getElementById('btn-backspace');
+
+let displayString = '0';
+
+function updateDisplay(value) {
+    displayElement.innerText = value;
+}
+
+function clearDisplay() {
+    displayString = '0';
+    updateDisplay(displayString);
+}
+
+function handleBackspace() {
+    if (displayString.length > 1) {
+        displayString = displayString.slice(0, -1);
+    } else {
+        displayString = '0';
+    }
+    updateDisplay(displayString);
+}
+
+function appendNumber(num) {
+    if (displayString === '0') {
+        displayString = num;
+    } else {
+        displayString += num;
+    }
+    updateDisplay(displayString);
+}
+
+function appendDecimal() {
+    if (!displayString.includes('.')) {
+        displayString += '.';
+        updateDisplay(displayString);
+    }
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        appendNumber(button.getAttribute('data-number'));
+    });
+});
+
+decimalButton.addEventListener('click', appendDecimal);
+clearButton.addEventListener('click', clearDisplay);
+backspaceButton.addEventListener('click', handleBackspace);s
